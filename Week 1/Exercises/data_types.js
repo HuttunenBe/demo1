45c886c5404a062a33da88e419b800525cf0ea69
HuttunenBe <s2500146@edu.bc.fi>
2025-01-16 14:57:52 +0200

    Something

---
+++ Week 1/Exercises/data_types.js
@@ -0,0 +1,18 @@
+let age = 25;
+const name = "John";
+const isStudent = true;
+let hobbies = ["movies", "reading", "playing", "dancing"];
+
+const info = {
+  schoolName: "Business College Helsinki",
+  groupName: 25,
+};
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
---
+++ Week 1/Exercises/variables_1.js
@@ -0,0 +1,6 @@
+let count = 0;
+count = count + 1;
+
+console.log(count);
+
+console.log(count + 1);
