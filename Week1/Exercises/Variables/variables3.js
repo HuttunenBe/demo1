3066d7802de6be8bd25a2d2eec568ad64fffeb73
HuttunenBe <s2500146@edu.bc.fi>
2025-01-18 15:48:04 +0200

    hello world

---
+++ Week1/Exercises/Variables/variables3.js
@@ -0,0 +1,5 @@
+let animalType = "Cats";
+let animalName = "Whiskers";
+
+let description = "My " + animalType + " is named " + animalName;
+console.log(description);
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 console.log("I will be developer");
 
 alert("Oh no");
+
+console.log("Hello");
